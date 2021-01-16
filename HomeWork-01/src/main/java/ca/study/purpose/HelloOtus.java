629e0460553521712977cb459eaa0df903759561
Vadim-16 <vadim16@hotmail.ca>
2021-01-15 19:27:04 -0700

    There we go again! added something: org.checkerframework.checker.nullness.qual.Nullable

--- HomeWork-01/src/main/java/ca/study/purpose/HelloOtus.java
+++ HomeWork-01/src/main/java/ca/study/purpose/HelloOtus.java
@@ -1,14 +1,16 @@
 package ca.study.purpose;
 
 
-import com.google.common.base.Function;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.function.Function;
 
 import static com.google.common.base.Functions.constant;
 
 public class HelloOtus {
 
     public static void main(String[] args) {
-        Function<Object, Integer> a = constant(5);
+        Function<Object, @Nullable Integer> a = constant(5);
         System.out.println("Hello world!");
         System.out.println(a);
     }
