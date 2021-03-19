235947d4aab75ec8400d16c86e6ae7d0409cc2f3
Vadim-16 <vadim16@hotmail.ca>
2021-03-18 22:02:00 -0600

    hw-12 v0.1

---
+++ HomeWork-12-WebServer/src/main/java/ca/study/purpose/MyServerDemo.java
@@ -0,0 +1,106 @@
+package ca.study.purpose;
+
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.SecurityHandler;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.security.Constraint;
+import ca.study.purpose.Servlets.Data;
+import ca.study.purpose.Servlets.PublicInfo;
+import ca.study.purpose.Servlets.PrivateInfo;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+
+/**
+ * Чтобы запустить пример из jar файла, положите в каталог с jar-файлом файл  realm.properties
+ */
+
+public class MyServerDemo {
+    private final static int PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        new MyServerDemo().start();
+    }
+
+    private void start() throws Exception {
+        Server server = createServer(PORT);
+        server.start();
+        server.join();
+    }
+
+    public Server createServer(int port) throws MalformedURLException {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new PublicInfo()), "/publicInfo");
+        context.addServlet(new ServletHolder(new PrivateInfo()), "/privateInfo");
+        context.addServlet(new ServletHolder(new Data()), "/data/*");
+
+        context.addFilter(new FilterHolder(new SimpleFilter()), "/*", null);
+
+        Server server = new Server(port);
+        server.setHandler(new HandlerList(context));
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{createResourceHandler(), createSecurityHandler(context)});
+        server.setHandler(handlers);
+        return server;
+    }
+
+    private ResourceHandler createResourceHandler() {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(false);
+        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
+
+        URL fileDir = MyServerDemo.class.getClassLoader().getResource("static");
+        if (fileDir == null) {
+            throw new RuntimeException("File Directory not found");
+        }
+        resourceHandler.setResourceBase(fileDir.getPath());
+        return resourceHandler;
+    }
+
+    private SecurityHandler createSecurityHandler(ServletContextHandler context) throws MalformedURLException {
+        Constraint constraint = new Constraint();
+        constraint.setName("auth");
+        constraint.setAuthenticate(true);
+        constraint.setRoles(new String[]{"user", "admin"});
+
+        ConstraintMapping mapping = new ConstraintMapping();
+        mapping.setPathSpec("/privateInfo/*");
+        mapping.setConstraint(constraint);
+
+        ConstraintSecurityHandler security = new ConstraintSecurityHandler();
+        //как декодировать стороку с юзером:паролем https://www.base64decode.org/
+        security.setAuthenticator(new BasicAuthenticator());
+
+        URL propFile = null;
+        File realmFile = new File("./realm.properties");
+        if (realmFile.exists()) {
+            propFile = realmFile.toURI().toURL();
+        }
+        if (propFile == null) {
+            System.out.println("local realm config not found, looking into Resources");
+            propFile = MyServerDemo.class.getClassLoader().getResource("realm.properties");
+        }
+
+        if (propFile == null) {
+            throw new RuntimeException("Realm property file not found");
+        }
+
+        security.setLoginService(new HashLoginService("MyRealm", propFile.getPath()));
+        security.setHandler(new HandlerList(context));
+        security.setConstraintMappings(Collections.singletonList(mapping));
+
+        return security;
+    }
+}
